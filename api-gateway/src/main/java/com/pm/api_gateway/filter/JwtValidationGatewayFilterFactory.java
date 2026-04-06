dc1946311cb1c7b80f849baf2796a9151639c8cb
Danish Shah <shanildanshah@gmail.com>
2026-04-06 20:05:16 +0530

    api issues resolved

---
+++ api-gateway/src/main/java/com/pm/api_gateway/filter/JwtValidationGatewayFilterFactory.java
@@ -0,0 +1,30 @@
+package com.pm.api_gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtValidationGatewayFilterFactory
+        extends AbstractGatewayFilterFactory<Object> {
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            String authHeader = exchange.getRequest()
+                    .getHeaders()
+                    .getFirst(HttpHeaders.AUTHORIZATION);
+
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            // TODO: validate the JWT token here
+
+            return chain.filter(exchange);
+        };
+    }
+}
